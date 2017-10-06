1621e6e81be81312ec7234848239de626b569b7d
anandskumar <anandskumar@gmail.com>
2017-10-06 13:55:18 -0700

    Added private tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,16 +7,17 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
   tasks() {
-  const instance = Template.instance();
-  if (instance.state.get('hideCompleted')) {
-    // If hide completed is checked, filter tasks
-    return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
-  }
-  // Otherwise, return all of the tasks
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
   incompleteCount() {
